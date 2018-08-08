0fd761cda3cbba5128ce8d6f5c26e6b847cfed4d
Linling  Miao <lmiao4@illinois.edu>
2018-08-08 02:12:16 -0500

    update contest96

--- Contest96/Contest96.java
+++ Contest96/Contest96.java
@@ -53,4 +53,37 @@ public class Contest96 {
         if(Character.isLetter(str[ptr-1])) return str[ptr-1]+"";
         else return str[0]+"";
     }
+
+    public int reachableNodes(int[][] edges, int m, int n) {
+        /** Weighted undirected graph */
+        int[][] graph = new int[n][n];
+        for(int i = 0; i<n; ++i) Arrays.fill(graph[i], -1);
+        for(int[] edge : edges){
+            graph[edge[0]][edge[1]] = edge[2];
+            graph[edge[1]][edge[0]] = edge[2];
+        }
+
+
+        /** Dijkstra Algorithm */
+        boolean[] marked = new boolean[n];
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        q.offer(new int[]{0, m});
+        int res = 0;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            if(marked[cur[0]]) continue;
+            marked[cur[0]] = true; res++;
+            for(int i = 0; i<n; ++i){
+                if(graph[cur[0]][i] > -1){
+                    if(cur[1] > graph[cur[0]][i] && !marked[i])
+                        q.offer(new int[]{i, cur[1]-graph[cur[0]][i]-1});
+                    int visited = Math.min(cur[1], graph[cur[0]][i]);
+                    graph[i][cur[0]] -= visited;
+                    res += visited;
+                }
+            }
+
+        }
+        return res;
+    }
 }
